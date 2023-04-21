7049b2f6fe54a2ac1cfc53b58b644d75a6130f8a
DevMinsuKim <cvnefr7704@gmail.com>
2023-04-21 15:39:26 +0900

    바잔3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Counter from "../components/Counter";
 export default function Home() {
   return (
     <>
-      <h1>홈페이지다!!</h1>
+      <h1>홈페이지다!! 버전3</h1>
       <Image
         src={"https://images.unsplash.com/photo-1441986300917-64674bd600d8"}
         alt="shop"
